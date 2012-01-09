04a24bc571d8f57d4bf1f45367113c23c8cfccd9
Stephan Hagemann <stephan.hagemann@gmail.com>
2012-01-08 23:49:43 -0700

    Added a couple of commands

--- redirects.rb
+++ redirects.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
 
+def parse_params(params)
+  parameters = params.keys.first.split
+  [parameters.shift, parameters.join(" ")]
+end
+
 get '/' do
-  "Hello World!"
+  command, parameters = parse_params(params)
+  new_url = case command
+            when 'g'
+              "https://www.google.com/search?q=#{parameters}"
+            when 'gr'
+              "https://www.google.com/search?tbs=qdr:m&q=#{parameters}"
+            when 'gim'
+              "http://www.google.de/search?tbm=isch&q=#{parameters}"
+            when 'amz'
+              "http://www.amazon.com/s?ie=UTF8&keywords=#{parameters}"
+            when 'wp'
+              "http://en.wikipedia.org/wiki/#{parameters}"
+            when 'wpg'
+              "http://de.wikipedia.org/wiki/#{parameters}"
+            when 'gm'
+              "http://maps.google.com/maps?q=#{parameters}"
+            when 'rg'
+              "http://rubygems.org/search?utf8=%E2%9C%93&query=#{parameters}"
+            when 'scr'
+              "http://www.scroogle.org/cgi-bin/nbbw.cgi?Gw=#{parameters}"
+            end
+  redirect to new_url
 end
--- redirects_test.rb
+++ redirects_test.rb
@@ -6,16 +6,59 @@ require 'rack/test'
 
 ENV['RACK_ENV'] = 'test'
 
-class RedirectsTest < Test::Unit::TestCase
+class String
   include Rack::Test::Methods
-
+  include Test::Unit::Assertions
   def app
     Sinatra::Application
   end
 
-  def test_it_says_hi
-    get '/'
-    assert last_response.ok?
-    assert_equal 'Hello World!', last_response.body
+
+  def should_redirect_to(url)
+    get self.to_s
+    follow_redirect!
+
+    assert_equal url, last_request.url
   end
+end
+
+class RedirectsTest < Test::Unit::TestCase
+
+
+  def test_redirect_to_google
+    '/?g+TEST'.should_redirect_to "https://www.google.com/search?q=TEST"
+  end
+
+  def test_redirect_to_google_recent_pages
+    '/?gr+TEST'.should_redirect_to "https://www.google.com/search?tbs=qdr%3Am&q=TEST"
+  end
+
+  def test_redirect_to_google_image_search
+    '/?gim+TEST'.should_redirect_to "http://www.google.de/search?tbm=isch&q=TEST"
+  end
+
+  def test_redirect_to_amazon
+    '/?amz+TEST'.should_redirect_to "http://www.amazon.com/s?ie=UTF8&keywords=TEST"
+  end
+
+  def test_redirect_to_english_wikipedia
+    '/?wp+TEST'.should_redirect_to "http://en.wikipedia.org/wiki/TEST"
+  end
+
+  def test_redirect_to_german_wikipedia
+    '/?wpg+TEST'.should_redirect_to "http://de.wikipedia.org/wiki/TEST"
+  end
+
+  def test_redirect_to_google_maps
+    '/?gm+TEST'.should_redirect_to "http://maps.google.com/maps?q=TEST"
+  end
+
+  def test_redirect_to_rubygems
+    '/?rg+TEST'.should_redirect_to "http://rubygems.org/search?utf8=%E2%9C%93&query=TEST"
+  end
+
+  def test_redirect_to_scroogle
+    '/?scr+TEST'.should_redirect_to "http://www.scroogle.org/cgi-bin/nbbw.cgi?Gw=TEST"
+  end
+
 end
