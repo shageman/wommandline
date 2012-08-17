e4e60fcb485999dcc1f1ec4735747f73e61b4a91
Stephan Hagemann <stephan.hagemann@gmail.com>
2012-08-17 07:32:43 -0600

    Added google mp3 and book search.

--- search_engine_query.rb
+++ search_engine_query.rb
@@ -6,12 +6,14 @@ class SearchEngineQuery
 
   MAPPING = {
     am: { url: "http://www.amazon.com/s?ie=UTF8&keywords=QUERY", description: "Amazon" },
+    book: { url: %{https://www.google.com/search?q=-inurl%3Ahtm+-inurl%3Ahtml+intitle%3A"index+of"+%2B("%2Febooks"%7C"%2Fbook")+%2B(chm%7Cpdf%7Czip)+%2B"QUERY"}, description: "Google for book search" },
     g: { url: "https://www.google.com/search?personal=0&q=QUERY", description: "Depersonalized Google" },
     gh: { url: "https://github.com/search?q=QUERY&type=Everything", description: "Github" },
     gim: { url: "http://www.google.de/search?personal=0&tbm=isch&q=QUERY", description: "Depersonalized Google image" },
     gm: { url: "http://maps.google.com/maps?q=QUERY", description: "Google Maps" },
     gr: { url: "https://www.google.com/search?personal=0&tbs=qdr:m&q=QUERY", description: "Depersonalized, recent Google" },
     leo: { url: "http://dict.leo.org/ende?search=QUERY", description: "Leo German-English Dictionary" },
+    mp3: { url: %{https://www.google.com/search?q=-inurl%3Ahtm+-inurl%3Ahtml+intitle%3A"index+of"+%2B("%2Fmp3")+%2B(chm%7Cpdf%7Czip)+%2B"QUERY"}, description: "Google for mp3 search" },
     r: { url: "http://apidock.com/ruby/search?query=QUERY", description: "Ruby APIdock" },
     ra: { url: "http://railsapi.com/doc/rails-v3.0.8rc1_ruby-v1.9.2/", description: "Rails API - how to search?" },
     rg: { url: "http://rubygems.org/search?utf8=%E2%9C%93&query=QUERY", description: "Rubygems" },
